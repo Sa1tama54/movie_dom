535148451288be93e3930462fa4492d6124fb5fb
Sulaiman <ibn.alauddin123@gmail.com>
2024-01-01 21:13:16 +0300

    Вывел отсортированный по алфавиту список фильмов на странице

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ const movieDB = {
 const advBlocks = document.querySelectorAll(".promo__adv > img");
 const genre = document.querySelector(".promo__genre");
 const promoBg = document.querySelector(".promo__bg");
+const interactiveList = document.querySelector(".promo__interactive-list");
 
 advBlocks.forEach((block) => {
   block.remove();
@@ -21,3 +22,16 @@ advBlocks.forEach((block) => {
 genre.textContent = "Драма";
 
 promoBg.style.backgroundImage = "url('../img/bg.jpg')";
+
+interactiveList.innerHTML = "";
+
+const movies = movieDB.movies.sort();
+
+movies.forEach((movie, i) => {
+  console.log(interactiveList);
+  interactiveList.innerHTML += `
+    <li class='promo__interactive-item'>
+      ${i + 1}. ${movie}
+      <div class='delete'></div>
+    </li>`;
+});
