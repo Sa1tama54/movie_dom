ae49dcf56f2e22e6ee9402d34e1ef92ebb0f9335
Sulaiman <ibn.alauddin123@gmail.com>
2024-01-01 21:10:13 +0300

    Изменил задний фон постера с фильмом на изображение bg.jpg

--- js/script.js
+++ js/script.js
@@ -12,9 +12,12 @@ const movieDB = {
 
 const advBlocks = document.querySelectorAll(".promo__adv > img");
 const genre = document.querySelector(".promo__genre");
+const promoBg = document.querySelector(".promo__bg");
 
 advBlocks.forEach((block) => {
   block.remove();
 });
 
 genre.textContent = "Драма";
+
+promoBg.style.backgroundImage = "url('../img/bg.jpg')";
