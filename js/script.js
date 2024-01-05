90bb00ab9223e4ab09af69b429baebc67f66f8d2
Sulaiman <ibn.alauddin123@gmail.com>
2024-01-05 20:10:24 +0300

    добавил событие DOMContentLoaded

--- js/script.js
+++ js/script.js
@@ -1,89 +1,91 @@
 "use strict";
 
-const movieDB = {
-  movies: [
-    "Логан",
-    "Лига справедливости",
-    "Ла-ла лэнд",
-    "Одержимость",
-    "Скотт Пилигрим против...",
-  ],
-};
+document.addEventListener("DOMContentLoaded", () => {
+  const movieDB = {
+    movies: [
+      "Логан",
+      "Лига справедливости",
+      "Ла-ла лэнд",
+      "Одержимость",
+      "Скотт Пилигрим против...",
+    ],
+  };
 
-const advBlocks = document.querySelectorAll(".promo__adv > img");
-const genre = document.querySelector(".promo__genre");
-const promoBg = document.querySelector(".promo__bg");
-const movieList = document.querySelector(".promo__interactive-list");
-const addForm = document.querySelector(".add");
-const addInput = addForm.querySelector(".adding__input");
-const favorite = addForm.querySelector("input[type=checkbox]");
+  const advBlocks = document.querySelectorAll(".promo__adv > img");
+  const genre = document.querySelector(".promo__genre");
+  const promoBg = document.querySelector(".promo__bg");
+  const movieList = document.querySelector(".promo__interactive-list");
+  const addForm = document.querySelector(".add");
+  const addInput = addForm.querySelector(".adding__input");
+  const favorite = addForm.querySelector("input[type=checkbox]");
 
-advBlocks.forEach((block) => {
-  block.remove();
-});
-
-genre.textContent = "Драма";
-
-promoBg.style.backgroundImage = "url('../img/bg.jpg')";
-
-const sortArr = (arr) => {
-  const newArr = [];
-
-  arr.forEach((item) => {
-    newArr.push(item.toLowerCase());
+  advBlocks.forEach((block) => {
+    block.remove();
   });
 
-  return newArr.sort();
-};
+  genre.textContent = "Драма";
 
-const generateMovieList = (movies) => {
-  movieList.innerHTML = "";
+  promoBg.style.backgroundImage = "url('../img/bg.jpg')";
 
-  const sortedMovies = sortArr(movies);
+  const sortArr = (arr) => {
+    const newArr = [];
 
-  sortedMovies.forEach((movie, i) => {
-    const li = document.createElement("li");
-    li.className = "promo__interactive-item";
-
-    const removingElement = document.createElement("div");
-    removingElement.className = "delete";
-
-    li.textContent = `${i + 1}. ${movie}`;
-    li.append(removingElement);
-
-    movieList.append(li);
-
-    removingElement.addEventListener("click", () => {
-      movies.splice(i, 1);
-      generateMovieList(movies);
+    arr.forEach((item) => {
+      newArr.push(item.toLowerCase());
     });
-  });
-};
 
-const addNewMovie = (e) => {
-  e.preventDefault();
+    return newArr.sort();
+  };
 
-  let newMovie = addInput.value.trim();
+  const generateMovieList = (movies) => {
+    movieList.innerHTML = "";
 
-  if (newMovie) {
-    if (newMovie.length > 21) {
-      newMovie = newMovie.slice(0, 21) + "...";
+    const sortedMovies = sortArr(movies);
+
+    sortedMovies.forEach((movie, i) => {
+      const li = document.createElement("li");
+      li.className = "promo__interactive-item";
+
+      const removingElement = document.createElement("div");
+      removingElement.className = "delete";
+
+      li.textContent = `${i + 1}. ${movie}`;
+      li.append(removingElement);
+
+      movieList.append(li);
+
+      removingElement.addEventListener("click", () => {
+        movies.splice(i, 1);
+        generateMovieList(movies);
+      });
+    });
+  };
+
+  const addNewMovie = (e) => {
+    e.preventDefault();
+
+    let newMovie = addInput.value.trim();
+
+    if (newMovie) {
+      if (newMovie.length > 21) {
+        newMovie = newMovie.slice(0, 21) + "...";
+      }
+
+      if (favorite.checked) {
+        console.log("Добавляем любимый фильм");
+      }
+
+      movieDB.movies.push(newMovie);
+      const sortedMovies = sortArr(movieDB.movies);
+
+      generateMovieList(sortedMovies);
     }
 
-    if (favorite.checked) {
-      console.log("Добавляем любимый фильм");
-    }
+    favorite.checked = false;
+    addInput.value = "";
+  };
 
-    movieDB.movies.push(newMovie);
-    const sortedMovies = sortArr(movieDB.movies);
+  addForm.addEventListener("submit", addNewMovie);
 
-    generateMovieList(sortedMovies);
-  }
-
-  favorite.checked = false;
-  addInput.value = "";
-};
-
-addForm.addEventListener("submit", addNewMovie);
-
-generateMovieList(movieDB.movies);
+  generateMovieList(movieDB.movies);
+});
